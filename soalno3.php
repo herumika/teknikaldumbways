e0616600c8dfce76842d360d819f50eb5dc53b39
herujatmikaputra <38009602+herumika@users.noreply.github.com>
2021-02-13 20:37:44 +0700

    Add files via upload

---
+++ soalno3.php
@@ -0,0 +1,21 @@
+<?php
+ $n=6;
+ for($i=1;$i<=$n;$i++)
+ {
+  echo "<center>";
+  for($j=1;$j<=$i;$j++)
+  {
+   if($j==1 || $j==$i)
+   {
+    $value[$i][$j]=1;
+    echo"x";
+   }
+   else
+   {
+    $value[$i][$j]=$value[$i-1][$j] + $value[$i-1][$j-1];
+   }
+   echo $value[$i][$j]." ";
+  }
+  echo "</center><br>";
+ }
+?>
